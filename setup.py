cbf8e935dabc7c05d7bb1aa5abcee8a3f7dc81d7
Nicholaswogan <wogan@uw.edu>
2021-05-08 17:42:13 -0700

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,20 @@ from setuptools import setup
 from subprocess import call
 
 sources = ['src/minpack.f90',
-           'src/minpack/dpmpar.f',
-           'src/minpack/enorm.f',
-           'src/minpack/fdjac2.f',
-           'src/minpack/lmdif.f',
-           'src/minpack/lmdif1.f',
-           'src/minpack/lmpar.f',
-           'src/minpack/qrfac.f',
-           'src/minpack/qrsolv.f']
+           'src/dpmpar.f',
+           'src/enorm.f',
+           'src/fdjac2.f',
+           'src/lmdif.f',
+           'src/lmdif1.f',
+           'src/lmpar.f',
+           'src/qrfac.f',
+           'src/qrsolv.f']
            
 flags = '-shared -fPIC -o NumbaMinpack/minpack.so -O3'.split()
     
 cmd = ['gfortran']+sources+flags
 call(cmd)
+call('rm minpack.mod'.split())
 
 setup(name = 'NumbaMinpack',
       packages=['NumbaMinpack'],
